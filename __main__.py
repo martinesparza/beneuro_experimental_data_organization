14058e5d35b5b36e77ff29c7817f1e2b68829a56
Bence Bagi <bagi.bence94@gmail.com>
2023-08-16 19:29:52 +0100

    Add some CLI functionality with typer

---
+++ __main__.py
@@ -0,0 +1,4 @@
+from beneuro_data import cli
+
+if __name__ == '__main__':
+    cli.app()
---
+++ src/beneuro_data/cli.py
@@ -0,0 +1,17 @@
+import typer
+
+from beneuro_data.data_transfer import Subject
+from beneuro_data.config import Config
+
+app = typer.Typer()
+
+@app.command()
+def validate_structure(subject_name: str):
+    subject = Subject(subject_name)
+    if subject.validate_local_session_folders('raw'):
+        typer.echo(f'{subject_name} looking good.')
+
+@app.command()
+def show_config():
+    config = Config()
+    typer.echo(config.json(indent = 4))
