ee67025a34f93b4de94d85fe7cbc1eee92e9b1e4
Bence Bagi <bagi.bence94@gmail.com>
2023-08-16 19:28:01 +0100

    Add BehavioralData class

--- src/beneuro_data/data_transfer.py
+++ src/beneuro_data/data_transfer.py
@@ -271,6 +271,33 @@ class EphysRecording:
         return sorting_KS3
 
 
-class BehavioralRecording:
+class BehavioralData:
     def __init__(self, session: Session):
+       self.session = session 
+
+       self.pycontrol_pattern = rf'^{session.subject.name}_{re.escape(session.date_str)}-'
+       #self.pycontrol_pattern += rf'{session.date.strftime("%Y-%m-%d")}-\d{{6}}' # this or the next one
+       self.pycontrol_pattern += r'.*' # this or the previous one. this if we don't care about the middle
+
+       self.pycontrol_pca_pattern = self.pycontrol_pattern + rf'_MotSen\d-(X|Y)\.pca'
+       self.pycontrol_txt_pattern = self.pycontrol_pattern + rf'\.txt'
+
+       # validate that txt and pca files are named correctly
+       for filename in os.listdir(self.get_path('local', 'raw')):
+           if os.path.splitext(filename)[1] == '.pca':
+               if re.match(self.pycontrol_pca_pattern, filename) is None:
+                   raise ValueError(f'Filename {filename} does not match expected pattern for PyControl .pca files')
+
+           if os.path.splitext(filename)[1] == '.txt':
+               if re.match(self.pycontrol_txt_pattern, filename) is None:
+                   raise ValueError(f'Filename {filename} does not match expected pattern for PyControl .txt files')
+
+           # don't care about any other files
+           if os.path.splitext(filename)[1] not in {'.pca', '.txt'}:
+               continue
+
+    def get_path(self, local_or_remote: str, processing_level: str) -> str:
+        return self.session.get_behavior_folder_path(local_or_remote, processing_level)
+
+    def get_pycontrol_py_file(self):
         raise NotImplementedError
