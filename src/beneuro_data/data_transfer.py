ca046545570eebd9253057e6e4172d255d1110cc
Bence Bagi <bagi.bence94@gmail.com>
2023-09-18 15:30:13 +0100

    Add functions to upload raw data from a session

--- src/beneuro_data/data_transfer.py
+++ src/beneuro_data/data_transfer.py
@@ -42,127 +42,182 @@ def sync_subject_dir(
         )
 
 
-# def upload_raw_session(
-#    session_path: Path,
-#    subject_name: str,
-#    local_root: Path,
-#    remote_root: Path,
-# ):
-#    pass
+def upload_raw_session(
+    local_session_path: Path,
+    subject_name: str,
+    local_root: Path,
+    remote_root: Path,
+    include_behavior: bool,
+    include_ephys: bool,
+):
+    if include_behavior:
+        upload_raw_behavioral_data(
+            local_session_path,
+            subject_name,
+            local_root,
+            remote_root,
+        )
+    if include_ephys:
+        upload_raw_ephys_data(
+            local_session_path,
+            subject_name,
+            local_root,
+            remote_root,
+        )
+
+    return True
 
 
 def upload_raw_behavioral_data(
-    session_path: Path,
+    local_session_path: Path,
     subject_name: str,
     local_root: Path,
     remote_root: Path,
 ):
+    processing_level = "raw"
+    remote_folders_created = []
+
+    # 0.1 make sure the session_path is actually a subdirectory of the local_root
+    if not local_session_path.is_relative_to(local_root):
+        raise ValueError(
+            f"Session path is not a subdirectory of the local root: {local_session_path}"
+        )
+    # 0.2 make sure the session_path has "raw" in it
+    if not ("raw" in local_session_path.parts):
+        raise ValueError(
+            f"Trying to upload a raw session, but the session's path does not contain 'raw': {local_session_path}"
+        )
     # 1. make sure locally the things are valid
-    validate_raw_behavioral_data_of_session(session_path, subject_name)
-    # TODO how about this previous one returning a dataclass with some diagnostics like pycontrol folder exists or not?
+    # TODO how about this one returning a dataclass with some diagnostics like pycontrol folder exists or not?
+    local_file_paths = validate_raw_behavioral_data_of_session(
+        local_session_path, subject_name, warn_if_no_pycontrol_py_folder=True
+    )
+
     # 2. check if there are remote files already
-    # 3. if not, create top level directory
+    remote_session_path = remote_root / local_session_path.relative_to(local_root)
+
+    remote_file_paths = [
+        remote_session_path / local_path.relative_to(local_session_path)
+        for local_path in local_file_paths
+    ]
+
+    for remote_path in remote_file_paths:
+        if remote_path.exists():
+            raise FileExistsError(f"Remote file already exists: {remote_path}")
+
+    # 3. if not, create the subject directory if needed
+    remote_subject_dir = remote_root / processing_level / subject_name
+    if not remote_subject_dir.exists():
+        sync_subject_dir(subject_name, processing_level, local_root, remote_root)
+        remote_folders_created.append(remote_subject_dir)
+
+    # if it doesn't exist, create the session's remote directory
+    if not remote_session_path.exists():
+        remote_session_path.mkdir()
+        remote_folders_created.append(remote_session_path)
+
     # 4. copy files in order
-    # 4.1 pycontrol output files
-    # 4.2 pycontrol py folder if exists
-    # 5. check if files are there and they are identical
+    files_already_copied = []
+    for local_path, remote_path in zip(local_file_paths, remote_file_paths):
+        try:
+            # for the .py file create the parent directory
+            if not remote_path.parent.exists():
+                assert remote_path.parent.is_relative_to(remote_session_path)
+                remote_path.parent.mkdir()
+
+            shutil.copy2(local_path, remote_path)
+            files_already_copied.append(remote_path)
+        except:
+            print(
+                f"Error copying {local_path} to {remote_path}. Rolling back changes made so far."
+            )
+            for p in files_already_copied:
+                try:
+                    p.unlink()
+                except:
+                    pass
+            for p in remote_folders_created:
+                try:
+                    shutil.rmtree(p)
+                except:
+                    pass
+
+            raise RuntimeError(
+                f"Error uploading raw behavioral data from {local_session_path}.\nSee stacktrace above."
+            )
+
+    # 5. check that the files are there and they are identical
+    remote_file_paths_there = validate_raw_behavioral_data_of_session(
+        remote_session_path, subject_name, warn_if_no_pycontrol_py_folder=False
+    )
+
+    if sorted(remote_file_paths_there) != sorted(remote_file_paths):
+        raise RuntimeError(
+            f"Something went wrong during uploading raw behavioral data for session {local_session_path}"
+        )
+
+    return True
 
 
-# @validate_argument("processing_level", ["raw", "processed"])
-# def sync_subject(subject: Subject, processing_level: str) -> bool:
-#     # 1. make sure locally the things are valid
-#     # 2. check if there are remote files already
-#     # 3. if not, create top level directory
-#     assert subject.local_store.has_folder(processing_level), "Subject has no local folder"
-#     assert not subject.remote_store.has_folder(
-#         processing_level
-#     ), "Subject already has remote folder"
+def upload_raw_ephys_data(
+    local_session_path: Path,
+    subject_name: str,
+    local_root: Path,
+    remote_root: Path,
+):
+    processing_level = "raw"
+    remote_folders_created = []
 
-#     if not subject.remote_store.has_folder(processing_level):
-#         subject.remote_store.create_folder(processing_level)
+    # 0.1 make sure the session_path is actually a subdirectory of the local_root
+    if not local_session_path.is_relative_to(local_root):
+        raise ValueError(
+            f"Session path is not a subdirectory of the local root: {local_session_path}"
+        )
+    # 0.2 make sure the session_path has "raw" in it
+    if not ("raw" in local_session_path.parts):
+        raise ValueError(
+            f"Trying to upload a raw session, but the session's path does not contain 'raw': {local_session_path}"
+        )
 
-#     if not subject.remote_store.has_folder(processing_level):
-#         raise FileNotFoundError(f"{subject.name} has no remote {processing_level} folder.")
+    # make sure locally the things are valid
+    validate_raw_ephys_data_of_session(local_session_path, subject_name)
 
-#     return True
+    # list the folders we want to upload
+    spikeglx_recording_folder_pathlib_pattern = "*_g?"
+    local_recording_folder_paths = list(
+        local_session_path.glob(spikeglx_recording_folder_pathlib_pattern)
+    )
 
+    # 2. check if there are remote files already
+    remote_session_path = remote_root / local_session_path.relative_to(local_root)
+    remote_recording_folder_paths_already_there = list(
+        remote_session_path.glob(spikeglx_recording_folder_pathlib_pattern)
+    )
+    if len(remote_recording_folder_paths_already_there) > 0:
+        raise FileExistsError(
+            f"Remote recording folders already exist: {remote_recording_folder_paths_already_there}"
+        )
 
-# def upload_raw_session(session: Session, dry_run: bool):
-#     assert session.has_folder("local"), "Session has no local folder"
+    remote_recording_folder_paths = [
+        remote_session_path / local_recording.relative_to(local_session_path)
+        for local_recording in local_recording_folder_paths
+    ]
 
-#     assert session.all_local_ephys_recordings_loaded(
-#         "raw"
-#     ), "Not all ephys recordings are loaded"
+    remote_folders_created = []
+    for local_path, remote_path in zip(
+        local_recording_folder_paths, remote_recording_folder_paths
+    ):
+        try:
+            shutil.copytree(local_path, remote_path, copy_function=shutil.copy2)
+            remote_folders_created.append(remote_path)
+        except:
+            print(
+                f"Error copying {local_path} to {remote_path}. Rolling back changes made so far."
+            )
 
-#     assert session.behavioral_data is not None, "Behavioral data is not loaded"
+            for p in remote_folders_created:
+                shutil.rmtree(p)
 
-#     if not session.subject.has_folder("remote", "raw"):
-#         # session.subject.create_folder("remote", "raw")
-#         sync_subject(session.subject, "raw")
-
-#     if not session.has_folder("remote", "raw"):
-#         session.create_folder("remote", "raw")
-
-#     local_remote_pairs = []
-
-#     # it can be that the session already has a remote folder with behavioral data but no ephys
-#     # in that case, shutil.copytree will fail because the destination folder already exists
-#     # so instead just loop throught the ephys recordings and copy them one by one
-#     for recording in session.ephys_recordings:
-#         if recording.has_folder("remote", "raw"):
-#             raise FileExistsError(
-#                 f"Remote folder already exists: {recording.get_path('remote', 'raw')}"
-#             )
-
-#         local_remote_pairs.append(
-#             (
-#                 recording.get_path("local", "raw"),
-#                 recording.get_path("remote", "raw"),
-#                 "folder",
-#             )
-#         )
-
-#     behavior = session.behavioral_data
-#     if behavior._pycontrol_task_folder_exists():
-#         local_pycontrol_py_folder_name = behavior._get_pycontrol_py_folder_path()
-#         pycontrol_py_file_name = behavior._pycontrol_task_py_file_name()
-#         local_remote_pairs.append(
-#             (
-#                 os.path.join(local_pycontrol_py_folder_name, pycontrol_py_file_name),
-#                 os.path.join(behavior.get_path("remote", "raw"), pycontrol_py_file_name),
-#                 "file",
-#             )
-#         )
-
-#     for extension in behavior._pycontrol_extensions:
-#         matching_filenames = [
-#             fname
-#             for fname in os.listdir(behavior.get_path("local", "raw"))
-#             if os.path.splitext(fname)[1] == extension
-#         ]
-#         for fname in matching_filenames:
-#             local_path = os.path.join(behavior.get_path("local", "raw"), fname)
-#             remote_path = os.path.join(behavior.get_path("remote", "raw"), fname)
-
-#             local_remote_pairs.append(
-#                 (
-#                     local_path,
-#                     remote_path,
-#                     "file",
-#                 )
-#             )
-
-#     for local_path, remote_path, filetype in local_remote_pairs:
-#         if dry_run:
-#             print(
-#                 local_path,
-#                 " -> ",
-#                 remote_path,
-#             )
-#         else:
-#             if filetype == "file":
-#                 shutil.copy(local_path, remote_path)
-#             elif filetype == "folder":
-#                 shutil.copytree(local_path, remote_path)
-#             else:
-#                 raise ValueError(f"Unknown filetype: {filetype}")
+            raise RuntimeError(
+                f"Error uploading raw ephys data from {local_session_path}.\nSee stacktrace above."
+            )
