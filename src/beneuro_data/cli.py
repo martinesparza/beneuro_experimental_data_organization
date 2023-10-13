de8bacbc0c3613285346555f04cfb1a5b59846b9
Bence Bagi <bagi.bence94@gmail.com>
2023-10-13 13:07:13 +0100

    Add a validate-session CLI command.
    Fixes #4

--- src/beneuro_data/cli.py
+++ src/beneuro_data/cli.py
@@ -12,6 +12,60 @@ app = typer.Typer()
 
 
 @app.command()
+def validate_session(
+    session_path: Annotated[
+        Path, typer.Argument(help="Path to session directory. Can be relative or absolute")
+    ],
+    subject_name: Annotated[
+        str,
+        typer.Argument(
+            help="Name of the subject the session belongs to. (Used for confirmation.)"
+        ),
+    ],
+    processing_level: Annotated[
+        str, typer.Argument(help="Processing level of the session. raw or processed.")
+    ] = "raw",
+    check_behavior: Annotated[
+        bool,
+        typer.Option(
+            "--check-behavior/--ignore-behavior", help="Check behavioral data or not."
+        ),
+    ] = True,
+    check_ephys: Annotated[
+        bool,
+        typer.Option("--check-ephys/--ignore-ephys", help="Check ephys data or not."),
+    ] = True,
+    check_videos: Annotated[
+        bool,
+        typer.Option("--check-videos/--ignore-videos", help="Check videos data or not."),
+    ] = True,
+):
+    """
+    Validate experiemental data in a given session.
+
+    E.g. to check all kinds of data in the current working directory which is supposedly a session of subject called M017:
+
+        `bnd validate-session . M017`
+    """
+    if processing_level != "raw":
+        raise NotImplementedError("Sorry, only raw data is supported for now.")
+
+    if all([not check_behavior, not check_ephys, not check_videos]):
+        raise ValueError("At least one data type must be checked.")
+
+    if not session_path.absolute().is_dir():
+        raise ValueError("Session path must be a directory.")
+
+    validate_raw_session(
+        session_path.absolute(),
+        subject_name,
+        check_behavior,
+        check_ephys,
+        check_videos,
+    )
+
+
+@app.command()
 def validate_sessions(
     subject_name: Annotated[str, typer.Argument(help="Subject name.")],
     processing_level: Annotated[
@@ -69,11 +123,6 @@ def show_config():
     Show the contents of the config file.
     """
     print(config.json(indent=4))
-
-
-# @app.command()
-# def print_path(session_path: Path):
-#    print(session_path.absolute())
 
 
 if __name__ == "__main__":
