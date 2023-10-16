5ef9838fc784020262e64f09d8357c428f2da537
Bence Bagi <bagi.bence94@gmail.com>
2023-10-16 15:46:56 +0100

    Add CLI command rename-videos

--- src/beneuro_data/cli.py
+++ src/beneuro_data/cli.py
@@ -6,6 +6,7 @@ from rich import print
 
 from beneuro_data.data_validation import validate_raw_session
 from beneuro_data.data_transfer import upload_raw_session
+from beneuro_data.video_renaming import rename_raw_videos_of_session
 from beneuro_data.config import config
 
 app = typer.Typer()
@@ -55,6 +56,8 @@ def validate_session(
 
     if not session_path.absolute().is_dir():
         raise ValueError("Session path must be a directory.")
+    if not session_path.absolute().exists():
+        raise ValueError("Session path does not exist.")
 
     validate_raw_session(
         session_path.absolute(),
@@ -62,6 +65,49 @@ def validate_session(
         check_behavior,
         check_ephys,
         check_videos,
+    )
+
+
+@app.command()
+def rename_videos(
+    session_path: Annotated[
+        Path, typer.Argument(help="Path to session directory. Can be relative or absolute")
+    ],
+    subject_name: Annotated[
+        str,
+        typer.Argument(
+            help="Name of the subject the session belongs to. (Used for confirmation.)"
+        ),
+    ],
+    processing_level: Annotated[
+        str, typer.Argument(help="Processing level of the session. raw or processed.")
+    ] = "raw",
+    verbose: Annotated[
+        bool,
+        typer.Option(help="Print the list of files that were renamed."),
+    ] = False,
+):
+    """
+    Rename the raw videos saved by Jarvis during a session to the convention we use.
+
+    Example usage:
+
+        `bnd rename-videos . M017 --verbose`
+
+        `bnd rename-videos /absolute/path/to/session M017 --verbose`
+    """
+    if processing_level != "raw":
+        raise NotImplementedError("Sorry, only raw data is supported for now.")
+
+    if not session_path.absolute().is_dir():
+        raise ValueError("Session path must be a directory.")
+    if not session_path.absolute().exists():
+        raise ValueError("Session path does not exist.")
+
+    rename_raw_videos_of_session(
+        session_path.absolute(),
+        subject_name,
+        verbose,
     )
 
 
