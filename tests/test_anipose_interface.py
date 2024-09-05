734d7c0dacb4a6ce7e6a24b6f887b4c9b5efa6f5
Bence Bagi <bagi.bence94@gmail.com>
2024-09-05 09:55:16 +0100

    Add quick test for AniposeInterface and a docstring

---
+++ tests/test_anipose_interface.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import pytest
+
+from beneuro_data.conversion.anipose_interface import AniposeInterface
+
+TEST_DIR_PATH = Path(__file__).parent
+
+
+@pytest.mark.parametrize("csv_name", ["M030_2024_04_12_09_40_3dpts_angles.csv"])
+def test_anipose_csv_loading(csv_name):
+    csv_path = TEST_DIR_PATH / "test_data" / csv_name
+    interface = AniposeInterface(csv_path)
+
+    for kp_name in AniposeInterface.keypoint_names:
+        for postfix in ["x", "y", "z"]:
+            assert f"{kp_name}_{postfix}" in interface.pose_data.columns
+
+    for angle_name in AniposeInterface.angle_names:
+        assert angle_name in interface.pose_data.columns
