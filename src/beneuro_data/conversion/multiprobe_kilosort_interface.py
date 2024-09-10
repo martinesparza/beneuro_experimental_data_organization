2c3d8e799132c731f4a0a9ca336dfb1b7fb8eb75
Martin Esparza <martinesparza22.me@gmail.com>
2024-09-10 18:16:52 +0100

    Added electrode location information

--- src/beneuro_data/conversion/multiprobe_kilosort_interface.py
+++ src/beneuro_data/conversion/multiprobe_kilosort_interface.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Optional
 
 import numpy as np
+import pandas as pd
 from neuroconv.datainterfaces import KiloSortSortingInterface
 from neuroconv.utils import DeepDict, FolderPathType
 from pynwb import NWBFile
@@ -26,6 +27,48 @@ def try_loading_trajectory_file(raw_recording_path):
         trajectory_dict = {probe: trajectory for probe, trajectory in probe_trajectory_pairs}
 
     return trajectory_dict
+
+
+def extract_channel_map_from_pinpoint_probe(filename, pinpoint_probe_name):
+    with open(filename, 'r') as file:
+        channel_map_str = file.read().strip()
+
+    channel_map_str = channel_map_str.strip('[]').split('","')
+    data_string = ''
+    for item in channel_map_str:
+        if pinpoint_probe_name in item:
+            data_string = item.split(":", 1)[1]
+            break
+
+    # Split the string by ";" to separate each entry
+    entries = data_string.split(";")
+    data = [dict(zip(["id", "area_number", "area_name", "area_color"], entry.split(",")))
+            for entry in entries]
+
+    df = pd.DataFrame(data)
+    return df
+
+
+def create_channel_map(location_information, raw_recording_path):
+
+    pinpoint_trajectory_file = list(raw_recording_path.glob('*channel_map.txt'))
+    if not pinpoint_trajectory_file:
+        warnings.warn(f"No channel map file from pinpoint found")
+        return
+
+    elif len(pinpoint_trajectory_file) > 1:
+        warnings.warn(f"Too many channel map files from pinpoint found")
+        return
+
+    channel_map = {}
+    for probe in location_information.keys():
+        pinpoint_probe_name = location_information[probe].split(":")[0]
+        channel_map[probe] = extract_channel_map_from_pinpoint_probe(
+            pinpoint_trajectory_file[0],
+            pinpoint_probe_name
+        )
+    
+    return channel_map
 
 
 class MultiProbeKiloSortInterface(KiloSortSortingInterface):
@@ -59,6 +102,10 @@ class MultiProbeKiloSortInterface(KiloSortSortingInterface):
             str(self.processed_recording_path).replace("processed", "raw"))
         meta_files = list(raw_recording_path.rglob("*/*ap.meta"))
         location_information = try_loading_trajectory_file(raw_recording_path)
+        if location_information is not None:
+            channel_map = create_channel_map(location_information, raw_recording_path)
+        elif location_information is None:
+            channel_map = None
 
         for meta_file, probe_name in zip(meta_files, self.probe_names):
             probe = pi.read_spikeglx(meta_file)  # Load probe object
@@ -89,13 +136,17 @@ class MultiProbeKiloSortInterface(KiloSortSortingInterface):
                 z = 0.0
                 # breakpoint()
                 contact_id = int(contact_id.split('e')[1:][0])
+                if channel_map is not None:
+                    contact_location = channel_map[probe_name].area_name[contact_id]
+                else:
+                    contact_location = None
                 nwbfile.add_electrode(
                     group=nwbfile.electrode_groups[probe_name],
                     x=float(x),
                     y=float(y),
                     z=z,
                     id=contact_id,
-                    location="??",  # TODO brain region
+                    location=contact_location,
                     reference=f"Local probe reference: Top of the probe",
                     enforce_unique_id=False
                 )
